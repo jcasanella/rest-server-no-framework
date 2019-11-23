e305b08fe5cc3c5484030547fce7ec1e816eb005
Jordi Casanella <jordikiter@hotmail.es>
2019-11-23 15:49:39 +0000

    Change package name main app

---
+++ src/main/java/com/revolut/rest/app/MainApp.java
@@ -0,0 +1,17 @@
+package com.revolut.rest.app;
+
+import com.revolut.rest.server.ServerRest;
+
+import java.io.IOException;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Starting the App");
+
+        try {
+            ServerRest.getInstance().startUp();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
